e21d29959d7c54f0716dca0c13dc85335028961f
Kevin Gov <kevin@kevingov.com>
2025-09-20 18:18:34 -0400

    Trigger Railway redeploy for QBO endpoints

--- app.py
+++ app.py
@@ -839,3 +839,4 @@ def show_tokens():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
+# QBO-Style Transaction Table - Updated Sat Sep 20 18:18:18 EDT 2025
